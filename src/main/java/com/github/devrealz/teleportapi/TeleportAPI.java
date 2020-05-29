7cec201cec78e35b2881df0cb85c3daeac8c82e8
devRealz - Casey <caseyollis03@gmail.com>
2020-05-29 08:38:46 -0400

    Initial Commit

--- src/main/java/com/github/devrealz/teleportapi/TeleportAPI.java
+++ src/main/java/com/github/devrealz/teleportapi/TeleportAPI.java
@@ -1,4 +1,32 @@
 package com.github.devrealz.teleportapi;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
 public class TeleportAPI {
+    Plugin plugin;
+    public TeleportAPI(Plugin pl) {
+        plugin = pl;
+    }
+
+    public void teleportPlayer(Player player, int delay, Location loc) {
+        plugin.getServer().getScheduler().runTaskLater(plugin,task ->{
+            player.sendMessage(ChatColor.YELLOW + "Teleporting you now.");
+            player.teleport(loc);
+        },20*delay);
+        player.getWorld().playEffect(loc, Effect.PORTAL_TRAVEL,delay);
+        player.sendMessage(ChatColor.AQUA + "Teleportation will commence shortly.");
+    }
+
+    public void teleportPlayer(Player player, int delay, Location loc,String onTeleportMessage) {
+        plugin.getServer().getScheduler().runTaskLater(plugin,task ->{
+            player.sendMessage(onTeleportMessage);
+            player.teleport(loc);
+        },20*delay);
+        player.getWorld().playEffect(loc, Effect.PORTAL_TRAVEL,delay);
+        player.sendMessage(ChatColor.AQUA + "Teleportation will commence shortly.");
+    }
 }
